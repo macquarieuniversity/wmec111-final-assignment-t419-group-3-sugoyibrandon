c0aa24b1c8ac66e92b046c3fb9aa74fe9e57dde5
sugoyibrandon <53545926+sugoyibrandon@users.noreply.github.com>
2019-09-06 10:10:15 +1000

    Final Unity Assignment Push LETZZ GOO

---
+++ WMEC111 Unity Assignment/Assets/_Game -- YOUR ASSETS GO HERE/Scripts/TriggerEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+public class TriggerEvent : MonoBehaviour
+{
+    public string hitTag = "Player";
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag(hitTag))
+        {
+            int activeScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(activeScene);
+        }
+    }
+}
+
+
